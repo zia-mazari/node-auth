c6aa0c35ab60e83db8928302b81c0ba4993b8e1d
zia_mazari <zia.mazari@icommunix.com>
2023-06-14 17:13:00 +0500

    add app port in .env

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const {connectDB} = require('./src/config/db');
 const routes = require('./src/routes');
 const app = express();
+require('dotenv').config();
 
 // Middleware
 app.use(express.json());
@@ -13,7 +14,7 @@ connectDB();
 app.use('/', routes);
 
 
-const port = 3000;
+const port = process.env.APP_PORT;
 app.listen(port, () => {
     console.log(`Server started at port: ${port}`);
 });
