304520791a64cc640b55d2eb3a67c0be841fb0a5
zia_mazari <zia.mazari@icommunix.com>
2023-06-15 15:36:30 +0500

    update: redisConfig file added

--- src/utils/helpers/tokenHelper.js
+++ src/utils/helpers/tokenHelper.js
@@ -1,61 +1,53 @@
-const redis = require('redis');
-const {v4: uuidv4} = require("uuid");
-const jwt = require("jsonwebtoken");
+const { v4: uuidv4 } = require('uuid');
+const jwt = require('jsonwebtoken');
+const {redisClient} = require('../../config/redisConfig');
 require('dotenv').config();
 
 
-// Create a Redis client
-const client = redis.createClient(/*{
-    host: 'redis_container', // Specify the host where Redis is running
-    port: 6380, // Specify the port mapped to the Redis container
-}*/);
-let isRedisConnected = false;
-
 exports.updateTokens = async (user, token, maxAllowedTokens = process.env.MAX_ALLOWED_LOGINS) => {
     try {
+        const client = await redisClient();
         user.tokens.push(token);
         if (user.tokens.length > maxAllowedTokens) {
-            // Remove the first token from the array & redis
+            // Remove the first token from the array & Redis
             const removedToken = user.tokens.shift();
             await client.del(removedToken.rtv);
         }
         const expiryTimeInSeconds = parseInt(process.env.TOKEN_LIFETIME_HOURS, 10) * 60 * 60;
         const v = await client.set(token.rtv, user.id, {
             EX: expiryTimeInSeconds,
-            NX: true
+            NX: true,
         });
         await user.save();
-        return {success: true, message: 'Tokens updated'};
+        return { success: true, message: 'Tokens updated' };
     } catch (exc) {
-        return {success: false, message: exc.message};
+        console.log('Exc to update token', exc);
+        return { success: false, message: exc.message };
     }
-}
+};
 
 exports.validateAccessToken = async (tokenVersion, payload) => {
-    return await client.exists(tokenVersion);
-}
+    try {
+        const client = await redisClient();
+        return await client.exists(tokenVersion);
+    } catch (exc) {
+        console.log('Error validating access token:', exc);
+        return false;
+    }
+};
 
 exports.generateAccessTokens = async (user) => {
     const tokenVersion = uuidv4();
-    const refreshToken = jwt.sign({user: {id: user.id}, token: {type: 'refresh', version: tokenVersion}},
-        process.env.JWT_SECRET, {expiresIn: '1w'});
-    const accessToken = jwt.sign({user: {id: user.id}, token: {type: 'access', version: tokenVersion}},
-        process.env.JWT_SECRET, {expiresIn: '8h'});
+    const refreshToken = jwt.sign(
+        { user: { id: user.id }, token: { type: 'refresh', version: tokenVersion } },
+        process.env.JWT_SECRET,
+        { expiresIn: '1w' }
+    );
+    const accessToken = jwt.sign(
+        { user: { id: user.id }, token: { type: 'access', version: tokenVersion } },
+        process.env.JWT_SECRET,
+        { expiresIn: '8h' }
+    );
 
-    return {accessToken, refreshToken, tokenVersion}
+    return { accessToken, refreshToken, tokenVersion };
 };
-
-(async () => {
-    if (!isRedisConnected) {
-        await new Promise((resolve, reject) => {
-            client.connect((error) => {
-                if (error) {
-                    reject(error);
-                } else {
-                    isRedisConnected = true;
-                    resolve(true);
-                }
-            });
-        });
-    }
-})();
